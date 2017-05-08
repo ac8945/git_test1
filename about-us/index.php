bba5313b3fffda7b97dd6aa5e557856c26d09743
Anthony C <ac@acme.com>
2017-05-08 03:06:38 -0400

    about us file

---
+++ about-us/index.php
@@ -0,0 +1,49 @@
+<?php 
+// meta tag description
+$meta_description = "Founded by Robert Piskulak - a European trained masonry professional with 25 yrs of experience. We specialize in stonework installation and restoration. ";
+
+//meta tag keywords
+$meta_keywords = "Art Stone, Robert Piskulak";
+
+// Page Title
+$page_title = "Masonry - Stone and Marble Work - Restoration - Art Stone";
+
+// CSS Stylesheet location
+$stylesheet_file = "../site.css";
+
+// JS files
+$html5shiv_file = "../js/html5shiv.js";
+
+// page for menu link highlighting
+$the_page = "aboutus";
+
+// Marble BG
+$marble_bg = "about-us-marble";
+
+// Header and Navigation
+include ("../header.php");
+?>
+
+<div class="main-container">
+	<div class="about-content cf">
+		<div class="about-us-text">
+			<p><b>Robert James</b>, Founder and President of ART STONE, graduated from the State School of Art in Kielce, Poland.</p>
+			<p>In 1990 he received his diploma in the field of SCULPTURE, PAINTING, DRAFTING, ART HISTORY AND ARTISTIC BUILDING DESIGN.</p>
+			<p>Immediately following his graduation, Robert worked in Italy for DALE NOGARE, a European leader in the marble and the natural stone market.</p>
+			<p>Robert has over <b>25 years of experience</b> with stone and marble work, including: sculpting, hand and machine/tool machining, and indoor and outdoor height installations.</p>
+			<p><b>ART STONE</b> is a NEW YORK based company specializing in stonework installation and restoration, and indoor and outdoor concepts. We work very hard to guarantee 110% customer satisfaction, and to create a trusting customer relationship for future projects.</p>
+			<p>We do both <b>commercial and residential work</b>. We are able to work on 1 or 250 bathrooms at the same time, install all shapes and sizes of marble and stone, as well as indoor and outdoor tiles. </p>
+			<p><b>Our motto is professionalism, accuracy and high quality.</b></p>
+			<p>We certainly hope to do our best work for you in the near future.</p>
+		</div>
+
+		<div class="about-us-photos">
+			<img src="../images/robert-james.jpg" alt="Robert James of Art Stone">
+		</div>
+	</div>
+</div>
+	
+<?php
+// Footer
+	include ("../footer.php");
+?>
