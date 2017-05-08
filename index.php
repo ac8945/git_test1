97f35badd28c93ef9a40460ef8e9382b74f32a87
Anthony C <ac@acme.com>
2017-05-08 00:15:05 -0400

    update 3

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php 
-// Page Title
-$page_title = "Anna Leonard Salon Solutions";
-
-// meta tag description
-$meta_description = "Anna Leonard Salon Solutions provides the Salon Industry with professional products and tools.";
-
-// CSS Stylesheet location
-$stylesheet_file = "site.css";
-
-// JS files
-$jquery_file = "js/jquery-1.11.2.min.js";
-
-$nav_file = "js/nav.js";
-
-$html5shiv_file = "js/html5shiv.min.js";
-$respond_file = "js/respond.min.js";
-
-$main_js_file = "js/main.js";
 
 // Header and Navigation
 include ("header.php");
@@ -28,17 +10,6 @@ include ("header.php");
 			alt="Salon scenes" class="full-width-image">
 </div>
 
-<div class="home-introduction-container cf">
-	<div class="home-introduction cf">
-		<h1>Beautiful Are Those Who Make Others Feel Beautiful</h1>
-	
-		<div class="home-intro-text">
-			<p>Anna Leonard Salon Solutions is a Customer Service-focused, Independent Representative serving licensed professionals in the Long Island, NY and Surrounding Tri-State areas.</p>
-			<p>Driven by a passion for the beauty industry and a belief in the power of making others feel and look their best, we offer the salon professional a variety of high quality, professional hair products.</p>
-			<p>Partnering with top distributors and manufacturers allows us to offer you a complete portfolio of diverse, professional hair products, cosmetics, tools and sundries with a simple, one call, one rep, solution based approach.</p>
-		</div>
-	</div>
-</div>
 
 <!-- SECTION -->
 <div class="section-headline-container">
@@ -58,71 +29,6 @@ include ("header.php");
 		</div>
 	</div>
 </div>
-
-<!-- SECTION Cosmetics -->
-
-<h1>Login Here</h1>
-
-<div class="section-headline-container">
-	<h2 class="section-headline">Cosmetics</h2>
-</div>
-
-<div class="home-content-container">
-	<div class="home-content cf">
-		<div class="two-col-a">
-			<img srcset="images/cosmetics-1520w.jpg 1520w,images/cosmetics-760w.jpg 760w"
-			sizes="(min-width: 701px) 50vw,100vw"
-			src="images/cosmetics-760w.jpg"
-			alt="assortment of cosmetics">
-		</div>
-		<div class="two-col-b">
-			<p>High Grade, professional quality, Cosmetics lines for the Salon Professional.</p>
-		</div>
-	</div>
-</div>
-
-
-<!-- SECTION -->
-<div class="section-headline-container">
-	<h2 class="section-headline">Sundries</h2>
-</div>
-
-<div class="home-content-container">
-	<div class="home-content cf">
-		<div class="two-col-a">
-			<img srcset="images/sundries-1520w.jpg 1520w,images/sundries-760w.jpg 760w"
-			sizes="(min-width: 701px) 50vw,100vw"
-			src="images/sundries-760w.jpg"
-			alt="hair salon sundries">
-		</div>
-		<div class="two-col-b">
-			<p>Salon Professional Sundries &amp; Tools including, foils, gloves, bowls, brushes, Pro tools, towels and much more!</p>
-		</div>
-	</div>
-</div>
-
-
-<!-- SECTION -->
-<div class="section-headline-container">
-	<h2 class="section-headline">About Us</h2>
-</div>
-
-<div class="home-content-container">
-	<div class="home-content cf">
-		<div class="two-col-a">
-			<img src="images/portrait.png" alt="owner's picture">
-		</div>
-		<div class="two-col-b">
-			<p>My passion for the beauty industry, strong commitment to customer service, and desire to offer Salon owners a diverse, high quality, portfolio of Professional Salon products, in a simplistic, one stop approach were the driving forces that launched my company.</p>
-			<p>I believe in building relationships not contacts. I believe in the power of Beauty Industry Professionals to positively impact the lives of their clients and I am, honored to support you!</p>
-		</div>
-	</div>
-</div>
-
-<div class="bottom-nav">
-	<a href="#page-top">Back to Top</a>
-</div>
-	
 	
 <?php
 // Footer
